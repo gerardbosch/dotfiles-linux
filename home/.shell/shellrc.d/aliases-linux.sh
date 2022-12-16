693bf023a9120e23be791ff354be2ce21bb48e2c
Gerard Bosch <gerard.bosch@gmail.com>
2022-12-16 23:02:23 +0100

    aliases-linux: Add `record-audio-out`

--- home/.shell/shellrc.d/aliases-linux.sh
+++ home/.shell/shellrc.d/aliases-linux.sh
@@ -4,3 +4,5 @@
 alias restic-backup-systemd-progress='journalctl -f --lines=50 -u restic-backup@default'
 alias restic-check-systemd-progress='journalctl -f -u restic-check@default'
 
+alias record-audio-out='ffmpeg -f pulse -ac 2 -ar 48000 -i alsa_output.pci-0000_00_1f.3.analog-stereo.monitor'
+
