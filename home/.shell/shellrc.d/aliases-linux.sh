0705dcd94d850967b1d1863ec049b8b59468e007
Gerard Bosch <gerard.bosch@gmail.com>
2023-09-06 12:00:27 +0200

    aliases-linux: Add record-audio-out-bluetooth

--- home/.shell/shellrc.d/aliases-linux.sh
+++ home/.shell/shellrc.d/aliases-linux.sh
@@ -5,5 +5,7 @@ alias restic-backup-systemd-progress='journalctl -f --lines=50 -u restic-backup@
 alias restic-check-systemd-progress='journalctl -f -u restic-check@default'
 
 alias record-audio-out='ffmpeg -f pulse -ac 2 -ar 48000 -i alsa_output.pci-0000_00_1f.3.analog-stereo.monitor'
+alias record-audio-out-bluetooth='ffmpeg -f pulse -ac 2 -ar 48000 -i bluez_output.5C_EB_68_00_5E_1E.1.monitor'
+
 alias f='nautilus . &disown' # file explorer
 
